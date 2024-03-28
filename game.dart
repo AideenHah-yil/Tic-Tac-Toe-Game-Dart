35a79debce0d01856048c1bbe79aacdf4dd868f3
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-28 15:53:56 +0300

    validating user input

--- game.dart
+++ game.dart
@@ -25,3 +25,32 @@ String getUserInput() {
     String input= stdin.readLineSync().trim().toUpperCase();
     return input;
 }
+
+// validate the user input
+bool isValidInput(String input) {
+    if (input.length != 2) {
+        return false;
+    }
+
+    String row = input.substring(0, 1);
+    String col = input.substring(1);
+
+    //a,b,c for rows and 1,2,3 for columns
+    if (!(row == 'A' || row == 'B' || row == 'C'
+    || col == '1' || col == '2' || col == '3')) {
+        return false;
+    }
+
+    //convert column to index
+    int colIndex = int.tryParse(col)! - 1;
+
+    //convert row letter to integer index
+    int rowIndex = row.codeUnitAt(0) - 'A'.codeUnitAt(0);
+    
+    //check if cell is empty
+    if (board[rowIndex][colIndex]!= '_') {
+        return false;
+    }
+
+return true;
+}
