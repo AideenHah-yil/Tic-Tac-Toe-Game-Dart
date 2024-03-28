726a7e269339a2359762940f33702341bac25b25
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-28 15:42:09 +0300

    working on player input

--- game.dart
+++ game.dart
@@ -8,11 +8,20 @@ List<List<String>> board = [
 
 ];
 
+//function to display the board
 void displayBoard() {
     for (int i = 0; i < board.length; i++) {
         for (int j = 0; j < board[i].length; j++) {
-            print(board[i][j]);
+            print(board[i][j] + ' ');
         }
         print('\n');
     }
+}
+
+// function to get user input
+
+String getUserInput() {
+    print("Enter your move (row and column, e.g , A1: )");
+    String input= stdin.readLineSync().trim().toUpperCase();
+    return input;
 }
